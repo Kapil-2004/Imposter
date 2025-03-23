f3b742efebdf7a5bec109efc11bab1a95f67daf7
Kapil-2004 <kapil202004@gmail.com>
2025-03-23 17:41:30 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { PopularPosts } from "../components/PopularPosts";
+import { AllPosts } from "../components/AllPosts";
 
 
-export  default function Home(){
-    return (
-        <h1>heloo this is home page</h1>
-      );
+export default function Home() {
+  return (
+    <div className="  ">
+      <div className="w-full  flex md:flex-row flex-col min-h-screen        pt-20">
+        <PopularPosts />
+        <AllPosts />
+    
+      
+        
+        
+      </div>
+    </div>
+  );
 }
